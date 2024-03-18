c85f0e7da096550a20dccbba5d6c01b27136988b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 23:16:52 +0300

    Create Part1-0002-Variables.dart

---
+++ Part1-0002-Variables.dart
@@ -0,0 +1,25 @@
+void main() {
+  
+  var myName = "Mustafa Buyukdereli"; //double quote
+
+  var thisYear = 2024;
+  
+  print(myName);
+  
+  int aNumber = 2023;
+  
+  String aProverb = 'A rolling stone gathers no moss.'; //single quote
+  
+  double priceThatProduct = 32.29;
+  
+  bool isCompleted = false;
+  
+  // $ and print
+  
+  int x = 303;
+  double y = 303.303;
+  
+  print("x + y = ${x + y}"); //x + y = 606.303
+
+}
+  
