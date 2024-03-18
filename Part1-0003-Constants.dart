b5be9377b9956de696de0471c0ef5ff089ea8fdb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 23:18:03 +0300

    Create Part1-0003-Constants.dart

---
+++ Part1-0003-Constants.dart
@@ -0,0 +1,18 @@
+void main() {
+  
+  //final
+  //final keyword is a run-time constant
+  
+  final int aNumber = 2023;
+  
+  print(aNumber);
+  
+  
+  //const
+  //const keyword is a compile-time constant
+  
+  const int aRandomNumber = 2023;
+  
+  print(aRandomNumber);
+  
+}
