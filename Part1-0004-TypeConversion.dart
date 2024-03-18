334b9253928ab1ff78e3294b1588cf77a6dd0766
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 23:19:14 +0300

    Create Part1-0004-TypeConversion.dart

---
+++ Part1-0004-TypeConversion.dart
@@ -0,0 +1,31 @@
+void main() {
+  
+  //number to number
+  
+  int a = 100;
+  
+  double b = 10.10;
+  
+  int c = b.toInt();
+  
+  double d = a.toDouble();
+  
+  
+  //number to string
+  
+  String e = a.toString(); //"100"
+  
+  String f = b.toString(); //"10.10"
+  
+  
+  //string to number
+  
+  String x = "125";
+  
+  int xNumber = int.parse(x); //125
+  
+  String y = "25.25";
+  
+  double yNumber = double.parse(y); //25.25
+   
+}
