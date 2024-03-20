65d6e5c535cf724ddc32a3b8bd8c5bffe6468375
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 21:53:36 +0300

    Create Part2-0002-If.dart

---
+++ Part2-0002-If.dart
@@ -0,0 +1,26 @@
+void main() {
+  
+  int personID = 11;
+  
+  String personTitle = "Founder";
+  
+  
+  if (personID == 1) {
+    print("You are the $personTitle.");
+  } else {
+    print("Who are you?");
+  }
+  
+  int year = 1990;
+  
+  if (year > 1980 && year <= 2000) {
+    print("Past, but you can remember!");
+  } else if (year > 2000 && year <= 2023) {
+    print("You should remember!");
+  } else {
+    print("Error:(");
+  }
+}
+  
+
+  
