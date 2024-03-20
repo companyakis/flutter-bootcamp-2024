9f2f80bb875b64437af7699b37568ed3d40c6af1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 22:02:40 +0300

    Create Part2-0008-Functions3.dart

---
+++ Part2-0008-Functions3.dart
@@ -0,0 +1,16 @@
+//functions.dart file
+
+class OurFunctions {
+  
+  //return
+
+  String sayHi() {
+    String greeting = "Hi there";
+    return greeting;
+  }
+
+  int sayThisYear() {
+    int year = 2023;
+    return year;
+  }
+}
