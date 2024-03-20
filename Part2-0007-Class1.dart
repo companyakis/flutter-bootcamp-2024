8cc6f38e4947e2f4c615267f3c8a46d94ff9f0e3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 21:58:12 +0300

    Create Part2-0007-Class1.dart

---
+++ Part2-0007-Class1.dart
@@ -0,0 +1,10 @@
+//country.dart file
+
+class Country {
+  String name; //late String
+  int population; //late int
+  bool isRepublic; //late bool
+
+  //constructor
+  Country({required this.name, required this.population, required this.isRepublic});
+}
