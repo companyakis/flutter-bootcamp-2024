0137a51b83ee6e933a74774dcbec4a2c0b0c941a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 22:04:11 +0300

    Create Part2-0008-Functions5.dart

---
+++ Part2-0008-Functions5.dart
@@ -0,0 +1,20 @@
+//functions.dart file
+
+class OurFunctions {
+  
+  //function parameters
+
+  void sayHi(String name) {
+    print("Hi $name!");
+  }
+
+  String sayWelcome(String name) {
+    String result = "Welcome $name";
+    return result;
+  }
+
+  int sumNumbers(int x, int y) {
+    int total = x + y;
+    return total;
+  }
+}
