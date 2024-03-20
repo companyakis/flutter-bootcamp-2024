8ccbd5c44c917061d9aa5b940e8bb437723d03ea
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 21:57:29 +0300

    Create Part2-0006-While.dart

---
+++ Part2-0006-While.dart
@@ -0,0 +1,22 @@
+void main() {
+  int counter = 0;
+
+  while (counter < 11) {
+    print("Counter_${counter + 1}: $counter");
+    counter++;
+  }
+
+  /*
+Counter_1: 0
+Counter_2: 1
+Counter_3: 2
+Counter_4: 3
+Counter_5: 4
+Counter_6: 5
+Counter_7: 6
+Counter_8: 7
+Counter_9: 8
+Counter_10: 9
+Counter_11: 10
+  */
+}
