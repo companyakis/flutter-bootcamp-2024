1d5d40c4d0ac249942a6aecde6c7d59d630f8082
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-11 20:36:34 +0300

    Create Part3-0007-Interface3.dart

---
+++ Part3-0007-Interface3.dart
@@ -0,0 +1,11 @@
+//interface usage sample
+
+import 'class_a.dart';
+
+void main() {
+  var a = ClassA();
+
+  print(a.year); //2023
+  a.method1(); //Hi there!
+  print(a.method2()); //I'm here!
+}
