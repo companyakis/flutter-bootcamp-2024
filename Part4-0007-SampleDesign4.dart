4afe062f95c597d9891fd80f844905dfc35a5757
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-12 11:43:15 +0300

    Create Part4-0007-SampleDesign4.dart

---
+++ Part4-0007-SampleDesign4.dart
@@ -0,0 +1,45 @@
+//myworks.dart file
+
+//main.dart is different
+
+import 'package:design_steps/colors.dart';
+import 'package:flutter/material.dart';
+
+class MyWorks extends StatefulWidget {
+  const MyWorks({super.key});
+
+  @override
+  State<MyWorks> createState() => _MyWorksState();
+}
+
+class _MyWorksState extends State<MyWorks> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Pizza",
+          style: TextStyle(
+            color: fontColor1, //from colors.dart file
+            fontFamily: "Pacifico", //we added!
+            fontSize: 20,
+          ),
+        ),
+        backgroundColor: mainColor, //from colors.dart file
+        centerTitle: true,
+      ),
+      body: const Center(
+        //horizontal centering
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //vertical centering
+          children: [
+            Text(
+              "Hi there!",
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
