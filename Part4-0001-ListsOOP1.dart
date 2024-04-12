273b69ce3b75d1b097d9bb58813fbefebde1ddae
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-12 10:29:00 +0300

    Create Part4-0001-ListsOOP1.dart

---
+++ Part4-0001-ListsOOP1.dart
@@ -0,0 +1,9 @@
+//students.dart file
+
+class Students {
+  int number;
+  String name;
+  String studentClass;
+
+  Students({required this.number, required this.name, required this.studentClass });
+}
