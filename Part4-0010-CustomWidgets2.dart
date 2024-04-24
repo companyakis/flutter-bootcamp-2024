e0cd00fff0dc332a9b888f0757bebceee33fdfbd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-24 09:29:22 +0300

    Create Part4-0010-CustomWidgets2.dart

---
+++ Part4-0010-CustomWidgets2.dart
@@ -0,0 +1,112 @@
+//myworks.dart file
+
+//main.dart is different
+
+import 'package:flutter/material.dart';
+import 'package:pizza/app_colors.dart';
+import 'package:pizza/buttonwidget.dart'; //custom widget file
+
+class MyWorks extends StatefulWidget {
+  const MyWorks({super.key});
+
+  @override
+  State<MyWorks> createState() => _MyWorksState();
+}
+
+class _MyWorksState extends State<MyWorks> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pizza",
+            style: TextStyle(
+                color: fontColor1, fontFamily: "Pacifico", fontSize: 20)),
+        backgroundColor: mainColor,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Beef Cheese",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: mainColor,
+                    fontWeight: FontWeight.bold)),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Image.asset("pictures/pizza.png"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ButtonWidget(buttonText: "Cheese"),
+                  ButtonWidget(buttonText: "Sausage"),
+                  ButtonWidget(buttonText: "Olive"),
+                  ButtonWidget(buttonText: "Pepper"),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  Text("30 min",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: fontColor2,
+                          fontWeight: FontWeight.bold)),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text("Delivery",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: mainColor,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Text(
+                    "Meet lover, you'll be happy! Be sure this pizza is awesome!",
+                    style: TextStyle(fontSize: 18, color: fontColor2),
+                    textAlign: TextAlign.center,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        Text("\$ 3.99",
+                            style: TextStyle(
+                                fontSize: 40,
+                                color: mainColor,
+                                fontWeight: FontWeight.bold)),
+                        Spacer(),
+                        SizedBox(
+                            width: 200,
+                            height: 50,
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Add to chart",
+                                  style: TextStyle(
+                                      color: fontColor1, fontSize: 20),
+                                ),
+                                style: TextButton.styleFrom(
+                                    backgroundColor: mainColor,
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(8))))))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
