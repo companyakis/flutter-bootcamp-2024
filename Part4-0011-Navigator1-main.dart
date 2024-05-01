747a4c9e4eb5b0e7ba9dda880acdc060ad5a5210
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-01 09:45:26 +0300

    Create Part4-0011-Navigator1-main.dart

---
+++ Part4-0011-Navigator1-main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:spapp/our_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Bootcamp 2024',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const OurApp(),
+    );
+  }
+}
