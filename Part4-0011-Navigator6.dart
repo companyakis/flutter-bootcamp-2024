34683e395d25c8768e94298c6b1b92c09aefd8ac
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-01 10:25:31 +0300

    Create Part4-0011-Navigator6.dart

---
+++ Part4-0011-Navigator6.dart
@@ -0,0 +1,38 @@
+//game_screen.dart file
+
+//push replacement
+
+import 'package:flutter/material.dart';
+import 'package:spapp/result_screen.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Game Screen")),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("This is game page"),
+          const SizedBox(height: 25),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ResultScreen()));
+              },
+              child: const Text("Result Page"))
+        ],
+      )),
+    );
+  }
+}
