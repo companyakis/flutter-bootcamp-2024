db8da869022ac0b3979c4fa1e7ab60e0b2d55659
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-23 10:58:25 +0300

    Create Part5-0010-Drawer-page_two.dart

---
+++ Part5-0010-Drawer-page_two.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PageTwo extends StatefulWidget {
+  const PageTwo({super.key});
+
+  @override
+  State<PageTwo> createState() => _PageTwoState();
+}
+
+class _PageTwoState extends State<PageTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("AI services will be here...", style: TextStyle(fontSize: 20)),
+        ],
+      )),
+    );
+  }
+}
