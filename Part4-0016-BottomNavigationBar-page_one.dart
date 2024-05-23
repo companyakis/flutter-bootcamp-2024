4e19352743ae14b5aab31afab9f961be13ab9a08
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-23 20:19:22 +0300

    Create Part4-0016-BottomNavigationBar-page_one.dart

---
+++ Part4-0016-BottomNavigationBar-page_one.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PageOne extends StatefulWidget {
+  const PageOne({super.key});
+
+  @override
+  State<PageOne> createState() => _PageOneState();
+}
+
+class _PageOneState extends State<PageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("DeFi and Web3 services will be here...",
+              style: TextStyle(fontSize: 20)),
+        ],
+      )),
+    );
+  }
+}
