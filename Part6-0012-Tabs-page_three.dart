09e688571e314ccd94bb00737c817aa369d95092
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-05-29 13:01:19 +0300

    Create Part6-0012-Tabs-page_three.dart

---
+++ Part6-0012-Tabs-page_three.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PageThree extends StatefulWidget {
+  const PageThree({super.key});
+
+  @override
+  State<PageThree> createState() => _PageThreeState();
+}
+
+class _PageThreeState extends State<PageThree> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("AR Services", style: TextStyle(fontSize: 25)),
+      ),
+    );
+  }
+}
