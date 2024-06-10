388e0902daa163619527d48e856b364d380956df
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-06-10 08:36:55 +0300

    Create Part6-0014-WebView3-main.dart

---
+++ Part6-0014-WebView3-main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse("https://github.com/companyakis"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: WebViewWidget(
+      controller: controller,
+    ));
+  }
+}
